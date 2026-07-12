1167953b0a0a62977ae0d625ea69668661b920ab
parthgoelcodes <parthgoel.codes@gmail.com>
2026-07-12 23:58:29 +0530

    Add files via upload

---
+++ D1 QP.py
@@ -0,0 +1,42 @@
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(2,21,2):
+#     print(i)
+#
+# for i in range(1,21,2):
+#     print(i)
+
+# sum = 0
+# for i in range(1,11):
+#     sum +=i
+# print(sum)
+#
+# n=7
+# for i in range(1,10+1):
+#     print(f"{n} x {i} = {n*i}")
+# while True:
+#     n=int(input("enter a number:-"))
+#     sum = 1
+#     for i in range(1,n+1):
+#         sum = sum * i
+#     print(sum)
+# count = 0
+# for i in range(2,101,2):
+#     count += 1
+# print(count)
+
+# count = 0
+# for i in range(1,100,2):
+#     count += 1
+# print(count)
+
+# print(True+False)
+# print(False+True+True)
+
+
+
+
