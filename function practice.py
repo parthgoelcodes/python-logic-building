2ba243c0d27392cdac29936fad21a38a1c22f9b6
parthgoelcodes <parthgoel.codes@gmail.com>
2026-07-18 22:34:13 +0530

    function practice

---
+++ function practice.py
@@ -0,0 +1,44 @@
+# def h():
+#     print("Hello World")
+#
+# h()
+##--------------------------------------------
+# def greet(name):
+#
+#     print(f"hello {name}")
+#
+#
+# greet("aman")
+# greet("ram")
+##---------------------------------------------------
+# def add(a,b):
+#     print(a+b)
+# add(12,3)
+# add(3,42)
+# add(15,32)
+# add(16,33)
+# add(125,36)
+# add(1245,34)
+##------------------------------------------------
+# def square(n):
+#     print(n*n)
+#     return(n*n)
+#
+# x=square(5)
+# print(x)
+##--------------------------------------
+from numpy.ma.core import true_divide
+
+
+# def is_even(n):
+#     if n % 2 ==0:
+#         print("True")
+#     else:
+#         print("False")
+#
+# is_even(5)
+# is_even(50)
+# is_even(25)
+# is_even(2)
+
+
